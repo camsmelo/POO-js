f0e99a6665bc2b0b929cd052a27e712ae4385481
Camila Melo <contato.camilamelo@outlook.com>
2021-01-03 14:36:01 -0300

    poo em javascript

---
+++ Conta/Conta.js
@@ -0,0 +1,49 @@
+export class Conta {
+    constructor(tipo, saldoInicial, cliente, agencia){
+        if(this.constructor == Conta){
+            throw new Error ('Você não deveria instanciar um objeto do tipo Conta diretamente, pois essa é uma classe abstrata')
+
+        }
+        this._saldo = saldoInicial;
+        this._cliente = cliente;
+        this._agencia = agencia;
+    }
+    
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this.cliente
+    }
+    
+    get saldo(){
+        return this.saldo;
+    }
+    sacar(valor){
+    }
+
+    _sacar(valor, taxa){
+        const valorSacado = taxa * valor;
+        if (this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+            return valorSacado;
+        }
+        return 0;
+    }
+
+    depositar(valor){
+        if(valor <= 100){
+            return;
+        }
+        this._saldo += valor;
+    }
+    
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+
+    }
+}
---
+++ Conta/index.js
@@ -0,0 +1,11 @@
+import {Cliente} from "./Conta/Cliente.js"
+import {Gerente} from "./Funcionario/Gerente.js"
+import {Diretor} from "./Funcionario/Diretor.js"
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
+
+const diretor = new Diretor("Rodrigo", 10000, 12345678900)
+const gerente = new Gerente("Ricardo", 5000, 12345678901)
+
+const estaLogado = SistemaAutenticacao.login(diretor, "123456789")
+
+console.log(estaLogado)
